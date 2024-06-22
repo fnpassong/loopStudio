b1177313cb201168f4c3835326dd80ea9033cb12
fnpassong <fa_pa01@hotmail.com>
2024-06-22 17:20:36 -0500

    <NavBar/> con sus estilos, spolo icon ham y el close

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import logo from '../assets/logo.svg';
 
 import '../scss/scssComponents/navbar.scss';
 
 const NavBar = () => {
+
+  function changeIcon() {
+    setOpen(!open);
+  }
+
+  const [open, setOpen] = useState(true);
+
   return (
-    <div className='bg-black'>
-      <h1 className='clr-very-gray'>Hola</h1>
-      <div className='logo'>
+    <nav className={open ? 'menu' : 'menu menu__hidden'}>
+      <div className="menu__wrapper">
+        <nav className="menu__nav">
+          <img className='menu__logo' src= { logo } alt="" />
+          <div className={open ? 'menu__list menu__notShow' : 'menu__list'}>
+              <NavLink className="menu__li">About</NavLink>
+              <NavLink className="menu__li">Careers</NavLink>
+              <NavLink className="menu__li">Events</NavLink>
+              <NavLink className="menu__li">Products</NavLink>
+              <NavLink className="menu__li">Support</NavLink>
+          </div>
 
-       <img src={logo} alt="" />
-
+          <div className={open ? 'icon-ham' : 'icon-close'} onClick={changeIcon}></div>
+        </nav>
       </div>
-    </div>
+    </nav>
+      
+
+      
+    
   )
 }
 
