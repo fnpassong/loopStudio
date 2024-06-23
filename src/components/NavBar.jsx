cf19d8e3302ae721a8d31f7a0fe24eaf25c83071
fnpassong <fa_pa01@hotmail.com>
2024-06-22 23:02:28 -0500

    global.scss => clase wrapper, <NavBar/> => .theSign

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,22 +13,33 @@ const NavBar = () => {
   const [open, setOpen] = useState(true);
 
   return (
-    <nav className={open ? 'menu' : 'menu menu__hidden'}>
-      <div className="menu__wrapper">
+    <div className={open ? 'menu' : 'menu menu__hidden'}>
+      <div className="menu__wrapper wrapper">
         <nav className="menu__nav">
           <img className='menu__logo' src= { logo } alt="" />
           <div className={open ? 'menu__list menu__notShow' : 'menu__list'}>
+            <ul className='menu__ul'>
               <NavLink className="menu__li">About</NavLink>
               <NavLink className="menu__li">Careers</NavLink>
               <NavLink className="menu__li">Events</NavLink>
               <NavLink className="menu__li">Products</NavLink>
               <NavLink className="menu__li">Support</NavLink>
+
+            </ul>
           </div>
 
           <div className={open ? 'icon-ham' : 'icon-close'} onClick={changeIcon}></div>
         </nav>
+        <div className="theSign">
+          <article className="sign clr-white">
+            <h1 className='sign__hero'>
+              immersive experiences that deliver
+            </h1>
+          </article>
+        </div>
       </div>
-    </nav>
+
+    </div>
       
 
       
